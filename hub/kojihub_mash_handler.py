63fd23825785a60ce6b430b47f184d2d6690f52a
Mathieu Bridon <bochecha@fedoraproject.org>
2013-12-10 14:09:49 +0800

    Initial import of the Hub plugin
    This is pretty much just a copy-paste of the code I had added to koji's
    Hub code, when I was attempting to implement this as a new builtin task.
    
    It certainly won't work, see the various TODO comments.

---
+++ hub/kojihub_mash_handler.py
@@ -0,0 +1,22 @@
+import koji
+
+
+def mash_tree(mash_target, build_tag, mash_opts, priority=None):
+    """Mash the repository tree"""
+    # TODO: How to log?
+    log.debug("Mashing '%s'..." % mash_target)
+
+    # TODO: How to get the `context`?
+    context.session.assertPerm("admin")
+
+    task_opts = {'channel': 'mash'}
+    if priority is not None:
+        task_opts["priority"] = koji.PRIO_DEFAULT + priority
+
+    # TODO: How to make the task?
+    return make_task('mashTree', [mash_target, build_tag, mash_opts],
+                     **task_opts)
+
+
+# Export the handler so it is accessible through the Hub's XMLRPC API
+mash_tree.exported = True
